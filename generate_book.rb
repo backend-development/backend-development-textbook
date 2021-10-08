b3bf6eb5044fb4633e65f97f7db1fe41455d222a
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2021-10-08 20:19:32 +0200

    github actions: debug $LOAD_PATH

--- generate_book.rb
+++ generate_book.rb
@@ -1,14 +1,14 @@
 pwd = __dir__
-$LOAD_PATH.unshift pwd
-$LOAD_PATH.unshift "#{pwd}/vendor/bundle"
-$LOAD_PATH.unshift "/home/runner/work/backend-development-textbook/backend-development-textbook/vendor/bundle"
+# $LOAD_PATH.unshift pwd
+# $LOAD_PATH.unshift "#{pwd}/vendor/bundle"
+# $LOAD_PATH.unshift "/home/runner/work/backend-development-textbook/backend-development-textbook/vendor/bundle"
 
 
-gems = Dir.glob('/home/runner/work/backend-development-textbook/backend-development-textbook/vendor/bundle/*')
-puts "the gems are:"
-gems.each {|g| puts g }
+# gems = Dir.glob('/home/runner/work/backend-development-textbook/backend-development-textbook/vendor/bundle/*')
+# puts "the gems are:"
+# gems.each {|g| puts g }
 
-puts "/gems"
+# puts "/gems"
 
 # This is a predicate useful for the doc:guides task of applications.
 def bundler?
@@ -19,6 +19,6 @@ end
 
 puts "I will require stuff from the LOAD PATH #{$LOAD_PATH}"
 
-# require 'rails_guides/markdown'
-# require 'rails_guides/generator'
-# RailsGuides::Generator.new.generate
+require 'rails_guides/markdown'
+require 'rails_guides/generator'
+RailsGuides::Generator.new.generate
