4b51b88e85b3ac60ed0e013841400f5ffd08f47d
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2021-10-08 14:33:27 +0200

    github actions: require redcarpet in generate_books.rb without exceptionhandling

--- generate_book.rb
+++ generate_book.rb
@@ -19,26 +19,7 @@ rescue LoadError
   # Guides generation from gems.
   gem "actionpack", '>= 3.0'
 end
-
-begin
-  require 'redcarpet'
-rescue Gem::LoadError
-  # This can happen if doc:guides is executed in an application.
-  $stderr.puts('Generating guides requires Redcarpet 2.1.1+.')
-  $stderr.puts(<<ERROR) if bundler?
-Please add
-
-  gem 'redcarpet', '~> 2.1.1'
-
-to the Gemfile, run
-
-  bundle install
-
-and try again.
-ERROR
-  exit 1
-end
-
+require 'redcarpet'
 require 'rails_guides/markdown'
 require "rails_guides/generator"
 RailsGuides::Generator.new.generate
