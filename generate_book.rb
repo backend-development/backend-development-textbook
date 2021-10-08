0dcbc338c2f3590911e1237b653806330b12854e
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2021-10-08 20:22:13 +0200

    github actions: debug $LOAD_PATH

--- generate_book.rb
+++ generate_book.rb
@@ -1,21 +1,7 @@
 pwd = __dir__
-# $LOAD_PATH.unshift pwd
+$LOAD_PATH.unshift pwd
 # $LOAD_PATH.unshift "#{pwd}/vendor/bundle"
 # $LOAD_PATH.unshift "/home/runner/work/backend-development-textbook/backend-development-textbook/vendor/bundle"
-
-
-# gems = Dir.glob('/home/runner/work/backend-development-textbook/backend-development-textbook/vendor/bundle/*')
-# puts "the gems are:"
-# gems.each {|g| puts g }
-
-# puts "/gems"
-
-# This is a predicate useful for the doc:guides task of applications.
-def bundler?
-  # Note that rake sets the cwd to the one that contains the Rakefile
-  # being executed.
-  File.exist?('Gemfile')
-end
 
 puts "I will require stuff from the LOAD PATH #{$LOAD_PATH}"
 
