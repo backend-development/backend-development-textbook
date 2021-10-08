fb7bc6656eb7efb3ce04edf25e91106655a095f3
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2021-10-08 20:24:58 +0200

    github actions: debug $LOAD_PATH

--- generate_book.rb
+++ generate_book.rb
@@ -1,6 +1,6 @@
 pwd = __dir__
 $LOAD_PATH.unshift pwd
-# $LOAD_PATH.unshift "#{pwd}/vendor/bundle"
+$LOAD_PATH.unshift "#{pwd}/vendor/bundle"
 # $LOAD_PATH.unshift "/home/runner/work/backend-development-textbook/backend-development-textbook/vendor/bundle"
 
 puts "I will require stuff from the LOAD PATH #{$LOAD_PATH}"
